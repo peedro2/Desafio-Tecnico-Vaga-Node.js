6efd1550f7a93661addcb13945243e0063585d0a
peedro2 <peedro21@gmail.com>
2025-05-05 01:50:19 -0300

    atualizar scripts

--- SqlPuro/server.js
+++ SqlPuro/server.js
@@ -4,10 +4,10 @@ import pedidoRotas from './src/routes/pedidoRotas.js';
 import errorHandler from './src/middlewares/errorHandler.js';
 
 
-dotenv.config(); 
-const app = express(); 
+dotenv.config();
+const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Pedidos da Cafeteria SQL Puro');
 });
